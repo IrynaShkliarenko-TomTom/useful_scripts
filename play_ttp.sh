753c305749f09959f5a01e7cd0f9093698a984a3
Iryna Shkliarenko <iryna.shkliarenko@tomtom.com>
2022-10-14 15:13:07 +0200

    Added script for playing ttp files

---
+++ play_ttp.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DEVICE_TTP_DIR=/sdcard/Android/data/com.tomtom.navapp/files/
+
+function usage {
+    echo ""
+    echo "To use the script:"
+    echo " ./play_ttp.sh <path to ttp file>"
+    echo ""
+}
+
+if [ $# -ne 1 ]; then
+	usage
+else
+	TTP_PATH=$1
+	echo ${TTP_PATH}
+	echo "Pushing '${TTP_PATH}' to '${DEVICE_TTP_DIR}'"
+	adb push "${TTP_PATH}" "${DEVICE_TTP_DIR}"
+	adb shell am startservice -n com.tomtom.navapp/com.tomtom.positioning.player.Player -a com.tomtom.positioning.player.START -e 'log_file' ${DEVICE_TTP_DIR}/route.ttp
+fi	
