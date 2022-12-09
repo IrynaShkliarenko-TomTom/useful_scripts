31fdb003bd040e56a568676b6f0c721a079bf292
Iryna Shkliarenko <iryna.shkliarenko@tomtom.com>
2022-12-09 17:37:40 +0100

    Added additional logging to TTP file player script

--- play_ttp.sh
+++ play_ttp.sh
@@ -5,7 +5,7 @@ DEVICE_TTP_DIR=/sdcard/Android/data/com.tomtom.navapp/files/
 function usage {
     echo ""
     echo "To use the script:"
-    echo " ./play_ttp.sh <path to ttp file>"
+    echo " ./play_ttp.sh <path to .ttp file>"
     echo ""
 }
 
@@ -16,5 +16,6 @@ else
 	echo ${TTP_PATH}
 	echo "Pushing '${TTP_PATH}' to '${DEVICE_TTP_DIR}'"
 	adb push "${TTP_PATH}" "${DEVICE_TTP_DIR}"
+	echo "Playing the file"
 	adb shell am startservice -n com.tomtom.navapp/com.tomtom.positioning.player.Player -a com.tomtom.positioning.player.START -e 'log_file' ${DEVICE_TTP_DIR}/route.ttp
 fi	
