696e5c417052589ae10bf5d93f2aeed528370bbb
Iryna Shkliarenko <iryna.shkliarenko@tomtom.com>
2022-12-14 16:41:16 +0100

    Added possibility to stop playing TTP file

--- play_ttp.sh
+++ play_ttp.sh
@@ -12,10 +12,16 @@ function usage {
 if [ $# -ne 1 ]; then
 	usage
 else
-	TTP_PATH=$1
-	echo ${TTP_PATH}
-	echo "Pushing '${TTP_PATH}' to '${DEVICE_TTP_DIR}'"
-	adb push "${TTP_PATH}" "${DEVICE_TTP_DIR}"
-	echo "Playing the file"
-	adb shell am startservice -n com.tomtom.navapp/com.tomtom.positioning.player.Player -a com.tomtom.positioning.player.START -e 'log_file' ${DEVICE_TTP_DIR}/route.ttp
+    if [ "$1" == stop ]; then
+        echo "Sending stop to '${PACKAGE}/com.tomtom.positioning.player.Player'"
+		adb shell am startservice -n com.tomtom.navapp/com.tomtom.positioning.player.Player -a com.tomtom.positioning.player.STOP
+	else
+		
+		TTP_PATH=$1
+		echo ${TTP_PATH}
+		echo "Pushing '${TTP_PATH}' to '${DEVICE_TTP_DIR}'"
+		adb push "${TTP_PATH}" "${DEVICE_TTP_DIR}"
+		echo "Playing the file"
+		adb shell am startservice -n com.tomtom.navapp/com.tomtom.positioning.player.Player -a com.tomtom.positioning.player.START -e 'log_file' ${DEVICE_TTP_DIR}/route.ttp
+	fi
 fi	
