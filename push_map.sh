55a89a13f84f54d22030472664db12ee1fcf5a54
IrynaShkliarenko-TomTom <iryna.shkliarenko@tomtom.com>
2023-11-13 16:48:19 +0100

    aded keystore path option

--- push_map.sh
+++ push_map.sh
@@ -5,7 +5,7 @@ DEVICE_MAP_DIR=/sdcard/Android/data/com.tomtom.navapp/files/maps/bundled
 function usage {
     echo ""
     echo "To use the script:"
-    echo " - sh ./push_map.sh *map path*
+    echo " - sh ./push_map.sh *map path* *keystore path*
     echo ""
 }
 
@@ -22,5 +22,12 @@ else
 		echo "Done"
 	else
 		echo "Failed to push '${MAPPATH}' to '${DEVICE_MAP_DIR}'"
+	fi
+	echo "Pushing '${KEYPATH}' to '${DEVICE_KYESTORE_DIR}'"
+	adb push "${KEYPATH}" "${DEVICE_KYESTORE_DIR}"
+	if [ $? -eq 0 ]; then
+		echo "Done"
+	else
+		echo "Failed to push '${KEYPATH}' to '${DEVICE_KYESTORE_DIR}'"
 	fi
 fi
