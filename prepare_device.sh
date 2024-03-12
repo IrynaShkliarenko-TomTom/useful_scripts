493a819b16503f325b24fcff0e6dc99ce8f22209
IrynaShkliarenko-TomTom <iryna.shkliarenko@tomtom.com>
2024-03-12 17:23:14 +0100

    added japan map option

--- prepare_device.sh
+++ prepare_device.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 DEVICE_MAP_DIR=/sdcard/Android/data/com.tomtom.navapp/files/maps/bundled/
-DEVICE_KYESTORE_DIR=/sdcard/Android/data/com.tomtom.navapp/files/keystore/
+DEVICE_KYESTORE_DIR=/sdcard/Android/data/com.tomtom.navapp/files/keystores/
 
-# adb install -g app-navkit1-debug.apk
+adb install -g app.apk
 adb shell am start -n com.tomtom.navapp/.MainActivity
 
 sleep 20
-
 adb shell "mkdir -p ${DEVICE_MAP_DIR}"
 adb shell "mkdir -p ${DEVICE_KYESTORE_DIR}"
 
@@ -19,10 +18,15 @@ adb push "/Users/shkliare/Documents/Maps/Poland/00000028" "${DEVICE_MAP_DIR}"
 adb push "/Users/shkliare/Documents/Maps/Poland/ROOT.NDS" "${DEVICE_MAP_DIR}"
 adb push "/Users/shkliare/Documents/Maps/Poland/NK_AUTO_DEV.NKS" "${DEVICE_KYESTORE_DIR}"
 
+# Japan Caruso2 Map
+# adb push "/Users/shkliare/Documents/Maps/japan_caruso/NDS_AutomotiveReference_2022.12_2.4.6_JPN-Caruso2-0823V1/DATA/00000028" "${DEVICE_MAP_DIR}"
+# adb push "/Users/shkliare/Documents/Maps/japan_caruso/NDS_AutomotiveReference_2022.12_2.4.6_JPN-Caruso2-0823V1/DATA/00000030" "${DEVICE_MAP_DIR}"
+# adb push "/Users/shkliare/Documents/Maps/japan_caruso/NDS_AutomotiveReference_2022.12_2.4.6_JPN-Caruso2-0823V1/DATA/ROOT.NDS" "${DEVICE_MAP_DIR}"
+# adb push "/Users/shkliare/Documents/Maps/japan_caruso/NK_AUTO_DEV.NKS" "${DEVICE_KYESTORE_DIR}"
+
 adb shell run-as com.tomtom.navapp touch /data/data/com.tomtom.navapp/files/first-run-wizard-completed
 adb shell am force-stop com.tomtom.navapp
 adb shell am start -n com.tomtom.navapp/.MainActivity
-
 
 if [ $? -eq 0 ]; then
 	echo "Done"
